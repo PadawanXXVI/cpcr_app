df9e4b1b2fe1ca9c129859c01702856bd5c22d43
Anderson de Matos Guimarães <anderson.guimaraes@novacap.df.gov.br>
2025-04-10 12:47:43 -0300

    feat(database): criar tabela 'processos' no banco de dados MySQL

--- scripts/criar_tabelas.py
+++ scripts/criar_tabelas.py
@@ -1,33 +1,35 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from configuracoes import DevelopmentConfig
 import mysql.connector
 
-# Conexão com o banco de dados MySQL
 conn = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='novacap',     
+    host=DevelopmentConfig.MYSQL_HOST,
+    user=DevelopmentConfig.MYSQL_USER,
+    password=DevelopmentConfig.MYSQL_PASSWORD,
+    database=DevelopmentConfig.MYSQL_DATABASE
 )
-
 cursor = conn.cursor()
 
-# Criação do banco e da tabela de usuários
-cursor.execute("CREATE DATABASE IF NOT EXISTS cpcr;")
-cursor.execute("USE cpcr;")
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS processos (
+    id_processo INT AUTO_INCREMENT PRIMARY KEY,
+    numero_processo VARCHAR(25) NOT NULL UNIQUE,
+    data_criacao_ra DATE NOT NULL,
+    data_entrada_cpcr DATE NOT NULL,
+    admin_regional VARCHAR(100) NOT NULL,
+    tipo_demanda VARCHAR(100) NOT NULL,
+    vistoria_completa TEXT,
+    diretoria_destino VARCHAR(10),
+    status_demanda VARCHAR(100) NOT NULL,
+    descricao_processo TEXT,
+    data_ultima_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+''')
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS usuarios (
-        id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-        nome VARCHAR(100) NOT NULL,
-        usuario VARCHAR(50) NOT NULL UNIQUE,
-        email VARCHAR(100) NOT NULL UNIQUE,
-        numero_celular VARCHAR(20),
-        senha_hash TEXT NOT NULL,
-        criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        senha_expira_em DATE
-    );
-""")
-
+print("✅ Tabela 'processos' criada com sucesso.")
 conn.commit()
 cursor.close()
 conn.close()
-
-print("Banco de dados 'cpcr' e tabela 'usuarios' criados com sucesso.")
