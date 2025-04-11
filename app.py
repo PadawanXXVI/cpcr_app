c0235eb1c3cca101e08876f23e6f1ce2c85ab69e
Anderson de Matos Guimarães <anderson.guimaraes@novacap.df.gov.br>
2025-04-11 12:21:58 -0300

    feat(flask): permite acesso externo via IP com host='0.0.0.0'

--- app.py
+++ app.py
@@ -392,4 +392,4 @@ def visualizacao():
                            lista_status=lista_status)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='10.115.14.149', port=5000, debug=True)
