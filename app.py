c85b5640d9af24fabd030fda08f19c87ab7e48e3
Anderson de Matos Guimarães <anderson.guimaraes@novacap.df.gov.br>
2025-04-13 13:56:52 -0300

    � Corrige importação de 'jsonify' para suporte à verificação AJAX de processo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from modelos import db, Usuario, Processo, Movimentacao, Status, Demanda, RegiaoAdministrativa
 from configuracoes import DevelopmentConfig
